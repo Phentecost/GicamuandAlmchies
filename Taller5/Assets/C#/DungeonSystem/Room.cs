447cca8747e5b630a242086444d7a4e144c4e59a
Phentecost <pablo.velezm@upb.edu.co>
2023-05-17 19:36:44 -0500

    cuartos y detalles

--- Taller5/Assets/C#/DungeonSystem/Room.cs
+++ Taller5/Assets/C#/DungeonSystem/Room.cs
@@ -68,22 +68,24 @@ namespace Code_DungeonSystem
 
         private void SpawnPortals() 
         {
-            if (secretRoom)
+            if (!boss)
             {
-                Instantiate(portal,pointA.transform.position,Quaternion.identity).GetComponent<Portals>().SetUp(ID);
-                Instantiate(portal, pointB.transform.position, Quaternion.identity).GetComponent<Portals>().SetUp(ID+1);
+                if (secretRoom)
+                {
+                    Instantiate(portal, pointA.transform.position, Quaternion.identity).GetComponent<Portals>().SetUp(ID);
+                    Instantiate(portal, pointB.transform.position, Quaternion.identity).GetComponent<Portals>().SetUp(ID + 1);
 
-            }
-            else
-            {
-                Instantiate(portal, pointB.transform.position, Quaternion.identity).GetComponent<Portals>().SetUp(ID);
-            }
+                }
+                else
+                {
+                    Instantiate(portal, pointB.transform.position, Quaternion.identity).GetComponent<Portals>().SetUp(ID);
+                }
 
-            if (relic != null)
-            {
-                Instantiate(relic, roomBounds.center + transform.position, Quaternion.identity);
+                if (relic != null)
+                {
+                    Instantiate(relic, roomBounds.center + transform.position, Quaternion.identity);
+                }
             }
-
         }
     }
 }
